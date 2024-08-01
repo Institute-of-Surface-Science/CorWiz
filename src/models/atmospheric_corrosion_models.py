094ac43616ada105854aa819a73307d0b9d0a71d
Aravinth Ravikumar <aravinth.ravikumar@hereon.de>
2024-08-01 09:01:08 +0200

    updated logos

--- src/models/atmospheric_corrosion_models.py
+++ src/models/atmospheric_corrosion_models.py
@@ -63,3 +63,39 @@ class i_the_prediction_of_atmospheric_corrosion_from_met(atmospheric_corrosion_m
         material_loss = self.eval_annual_corrosion()*time**self.evaluate_exponent()
         return material_loss
 
+
+class iso_9223(atmospheric_corrosion_model):
+    
+    '''
+
+    '''
+    def __init__(self, parameters):
+        atmospheric_corrosion_model.__init__(self)
+        self.model_name = 'ISO 9223:2012 and ISO 9224:2012'
+        self.article_identifier = ['din-en-iso-92232012-05', 'din-en-iso-92242012-05']
+        self.steel = "Unalloyed Steel"
+        self.p = parameters  # [0 - Pd, 1 - Sd, 2 - RH, 3 - fst, 4- temp, 5 - b]
+
+    
+    def eval_corrosion_speed(self):
+
+        if self.p[4] <= 10:
+            fst = 0.15*(self.p[4] - 10)
+        else:
+            fst = -0.054*(self.p[4] - 10)
+
+        corrosion_speed = 1.77*self.p[0]**0.52*np.e**(0.02*self.p[2] + fst) + 0.102*self.p[1]**0.62*np.e**(0.033*self.p[2] + 0.04*self.p[4])
+        return corrosion_speed
+
+
+    def eval_material_loss(self, time):
+        
+        corrosion_speed = self.eval_corrosion_speed()
+        
+        if time < 20:
+            material_loss = self.p[5]*corrosion_speed*time**(self.p[5] - 1)
+
+        else:
+            material_loss = corrosion_speed*(20**self.p[5] + self.p[5]*20**(self.p[5] - 1)*(time - 20))
+
+        return material_loss
