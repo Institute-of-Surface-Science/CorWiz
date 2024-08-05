d11d64f80e93cb69739f06fb9f8c1effee52916e
Sven Berger <berger.sven@gmail.com>
2024-08-05 16:29:48 +0200

    update files

--- web_app/website.py
+++ web_app/website.py
@@ -59,11 +59,9 @@ with header:
             "The goal is to create a comprehensive database of corrosion knowledge, including both structured and unstructured data such as research articles. Currently, engineers face challenges in selecting appropriate models and accessing relevant data for corrosion simulations. CorWiz aims to address this by providing a user-friendly web tool that offers engineers access to corrosion data, models, and relevant literature. By inputting parameters such as substrate material and environmental conditions, users can obtain corrosion grades, simulation results, and other structured data. The development of CorWiz involves data scraping, processing, and quality assessment, which will be facilitated by tools developed for the Kadi4Mat research data management platform. This project aims to streamline the design process, raise awareness of potential corrosion issues, and contribute to more cost-effective and eco-friendly design practices.")
         st.write("[Learn more at >](https://www.hereon.de/institutes/surface_science/projects/112600/index.php.en)")
     with right_column:
-        # st_lottie(lottie_animation, height=100, key="coding")
-        # st.image('../data/animations/Presentation2v2.gif')
         display_logo(
-            url="https://www.http://corwiz.xyz/",
-            img_src="./app/static/logos/Presentation2v2.gif",
+            url="http://corwiz.xyz/",
+            img_src="./app/static/logos/banner.gif",
             width="80%",
             alt_text="Logo of Corwiz",
         )
