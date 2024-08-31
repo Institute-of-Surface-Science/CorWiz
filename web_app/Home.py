c48e9d7b3381087283acf4d451404c5792f107c8
Sven Berger <berger.sven@gmail.com>
2024-08-31 17:22:55 +0200

    optimize information pages

--- web_app/Home.py
+++ web_app/Home.py
@@ -3,11 +3,6 @@ import streamlit as st
 from header import add_header
 from footer import add_footer
 from model_view import model_view
-from helper import local_css
-
-st.set_page_config(page_title="CorWiz", page_icon=":material/rainy:", layout="wide", initial_sidebar_state="auto")
-
-local_css('style/style.css')
 
 add_header()
 
