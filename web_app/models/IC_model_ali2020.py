ebfde794cd929377305f6d66b463319d2ae92fc9
Aravinth Ravikumar <aravinth.ravikumar@hereon.de>
2024-09-06 10:44:46 +0200

    removed experimental results from the model

--- web_app/models/IC_model_ali2020.py
+++ web_app/models/IC_model_ali2020.py
@@ -18,19 +18,17 @@ class Ali2010Model(CorrosionModel):
     """
 
     DATA_FILE_PATHS = {'table_3': '../data/tables/ali2020_table_3.csv',
-                      'table_4': '../data/tables/ali2020_table_4.csv'
     }
 
     def __init__(self, json_file_path: str):
         super().__init__(json_file_path=json_file_path, model_name='Ali2010Model')
-        self.table_3, self.table_4 = self._load_data()
+        self.table_3 = self._load_data()
         self.parameters: Dict[str, float] = {}
 
     def _load_data(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         """Loads the relevant data tables for the ISO 9224 model."""
         return (
             pd.read_csv(self.DATA_FILE_PATHS['table_3'], header=None),
-            pd.read_csv(self.DATA_FILE_PATHS['table_4'], header=None),
         )
 
 
@@ -74,48 +72,4 @@ class Ali2010Model(CorrosionModel):
         material_loss = (0.00006 * self.parameters['C'] + 0.0008) * time*24*365 + self.parameters['b']
         
         return material_loss
-
-    def _bilinear_interpolation(self, x, y, values, nacl_concentration, time):
-        """Perform bilinear interpolation."""
-        x1 = np.searchsorted(nacl_concentration, x) - 1
-        x2 = x1 + 1
-        y1 = np.searchsorted(time, y) - 1
-        y2 = y1 + 1
-        
-        x1 = np.clip(x1, 0, len(nacl_concentration) - 2)
-        x2 = np.clip(x2, 1, len(nacl_concentration) - 1)
-        y1 = np.clip(y1, 0, len(time) - 2)
-        y2 = np.clip(y2, 1, len(time) - 1)
-        
-        x1_val = nacl_concentration[x1]
-        x2_val = nacl_concentration[x2]
-        y1_val = time[y1]
-        y2_val = time[y2]
-        
-        Q11 = values[y1, x1]
-        Q21 = values[y1, x2]
-        Q12 = values[y2, x1]
-        Q22 = values[y2, x2]
-        
-        return (Q11 * (x2_val - x) * (y2_val - y) +
-                Q21 * (x - x1_val) * (y2_val - y) +
-                Q12 * (x2_val - x) * (y - y1_val) +
-                Q22 * (x - x1_val) * (y - y1_val)) / ((x2_val - x1_val) * (y2_val - y1_val))
-
-
-    def eval_material_loss_exp(self, time:float) -> float:
-        """Evaluates the material loss over time based on the experimental data.
-        
-        Args:
-            time (float): The time duration in years.
-            
-        Returns:
-            float: The calculated material loss.
-        """
-    
-        return self._bilinear_interpolation(self.parameters['C'], 
-                                       time, 
-                                       self.table_4.iloc[2:, 1:].astype(float).to_numpy(), 
-                                       self.table_4.iloc[1, 1:].astype(float).to_numpy(), 
-                                       self.table_4.iloc[2:, 0].astype(float).to_numpy() / 24 / 365)
 
