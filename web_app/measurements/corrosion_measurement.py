82f3201a0a53b2c66dd367c95436c240f139ad68
Sven Berger <berger.sven@gmail.com>
2024-09-09 17:23:18 +0200

    remove unneeded stuff

--- web_app/measurements/corrosion_measurement.py
+++ web_app/measurements/corrosion_measurement.py
@@ -36,65 +36,6 @@ class CorrosionMeasurement(Measurement):
         raise NotImplementedError("This method should be implemented by subclasses.")
 
 
-class CorrosionProcessTypeError(Exception):
-    """Custom exception for invalid corrosion process type."""
-    pass
-
-
-def get_corrosion_process_type(measurements: Union[Measurement, List[Measurement]]) -> Union[Tuple[Measurement, str], List[Tuple[Measurement, str]]]:
-    """
-    Determines the corrosion process type for a single measurement or a list of measurements based on their tags.
-
-    This function inspects the tags associated with each measurement to identify the corrosion process type.
-    A valid tag must contain exactly three words, with the second-to-last word being "corrosion" and
-    the last word being "measurement". The corrosion process type is then extracted from the first word in the tag.
-
-    Args:
-        measurements (Union[Measurement, List[Measurement]]): A single measurement instance or a list of measurement instances to check.
-
-    Returns:
-        Union[Tuple[Measurement, str], List[Tuple[Measurement, str]]]:
-            - If a single measurement is passed, returns a tuple (measurement, process_type).
-            - If a list of measurements is passed, returns a list of tuples where each tuple is (measurement, process_type).
-
-    Raises:
-        CorrosionProcessTypeError: If no valid tag is found in a measurement, i.e., no tag contains exactly
-                                   three words with the last two words being "corrosion measurement".
-        TypeError: If the input is neither a Measurement instance nor a list of Measurement instances.
-    """
-
-    def determine_type(measurement: Measurement) -> str:
-        """Helper function to determine the corrosion process type for a single measurement."""
-        for tag in measurement.tags:
-            words = tag.split()
-            # The tag must have exactly three words, with the last two being "corrosion" and "measurement"
-            if len(words) == 3 and words[-2] == "corrosion" and words[-1] == "measurement":
-                process_type = words[0] + " corrosion"  # Extract and format the process type
-                return process_type
-
-        raise CorrosionProcessTypeError("Measurement does not have any valid corrosion process tags.")
-
-    if isinstance(measurements, Measurement):
-        # Handle the case where a single measurement is passed
-        process_type = determine_type(measurements)
-        return measurements, process_type
-
-    elif isinstance(measurements, list):
-        # Handle the case where a list of measurements is passed
-        measurement_process_pairs = []
-        for measurement in measurements:
-            try:
-                process_type = determine_type(measurement)
-                measurement_process_pairs.append((measurement, process_type))
-            except CorrosionProcessTypeError as e:
-                print(f"Error processing measurement {measurement.name}: {e}")
-
-        return measurement_process_pairs
-
-    else:
-        raise TypeError("Input must be a Measurement instance or a list of Measurement instances.")
-
-
 def load_corrosion_measurements_from_directory(directory_paths: Union[str, List[str]], measurement_classes: Dict[str, Any]) -> List[CorrosionMeasurement]:
     """Loads all corrosion measurements from JSON files in the specified directory or directories.
 
--- web_app/model_view.py
+++ web_app/model_view.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import io
 from models import *
+from measurements import *
 from typing import List
 from plot_view import generate_plot, display_plot_html
 
@@ -26,12 +27,11 @@ def display_model_info(model: Model) -> None:
         st.markdown(f"#### Model Notes: \n{model.special_note}")
 
 
-
-def display_model_view(container):
+def display_model_view(page_container):
     """
     Displays the model view interface for selecting and analyzing corrosion mass loss models.
     Args:
-        container (st.container): The Streamlit container in which the model view interface will be displayed.
+        page_container (st.container): The Streamlit container in which the model view interface will be displayed.
     """
     # Load available corrosion models from the directories
     model_directories = [
@@ -40,13 +40,20 @@ def display_model_view(container):
     ]
     models = load_corrosion_models_from_directory(model_directories, corrosion_models)
 
+    measurement_directories = [
+        '../data/kadi4mat_json/immersion_corrosion_measurements/',
+    ]
+
+    measurements = load_corrosion_measurements_from_directory(measurement_directories)
+
     # Initialize plot_data in session state if it doesn't exist
     if 'plot_data' not in st.session_state:
         st.session_state.plot_data = []
 
     fig = None
 
-    with container:
+    with page_container:
+
         st.write("---")
         st.header("Corrosion Mass Loss Models")
 
@@ -59,59 +66,75 @@ def display_model_view(container):
             plot_column, selection_column = st.columns((1, 1))
 
             with selection_column:
-                # Selection for corrosion type
-                corrosion_types = sorted({process_type for _, process_type in model_process_pairs})
-                selected_corrosion_type = st.selectbox('**Corrosion Type**', corrosion_types, key="corrosion_type")
+                selection_top, selection_bottom = st.container(), st.container()
 
-                # Filter models based on the selected corrosion type
-                filtered_models = [
-                    model for model, process_type in model_process_pairs if process_type == selected_corrosion_type
-                ]
-                selected_model = st.selectbox(
-                    '**Model**', filtered_models,
-                    format_func=lambda model: f"{model.name} ({model.kadi_identifier})",
-                    key="model_select"
-                )
+                with selection_top:
+                    model_tab, measurement_tab, wizard_tab = st.tabs(["Model Selection", "Measurements", "Wizard"])
 
-                # Input for time range (duration in years)
-                time_range = st.number_input('Enter duration [years]:', min_value=2.5, max_value=100.0, step=2.5,
-                                             key="time_range")
+                    with model_tab:
+                        # Selection for corrosion type
+                        corrosion_types = sorted({process_type for _, process_type in model_process_pairs})
+                        selected_corrosion_type = st.selectbox('**Corrosion Type**', corrosion_types,
+                                                               key="corrosion_type")
 
-                # Display the selected model's parameters
-                selected_model.display_parameters()
+                        # Filter models based on the selected corrosion type
+                        filtered_models = [
+                            model for model, process_type in model_process_pairs if
+                            process_type == selected_corrosion_type
+                        ]
+                        selected_model = st.selectbox(
+                            '**Model**', filtered_models,
+                            format_func=lambda model: f"{model.name} ({model.kadi_identifier})",
+                            key="model_select"
+                        )
 
-                # Display the location associated with the model on the map
-                if selected_model.model_coordinates is not None:
-                    st.map(selected_model.model_coordinates)
+                        # Input for time range (duration in years)
+                        time_range = st.number_input('Enter duration [years]:', min_value=2.5, max_value=100.0,
+                                                     step=2.5,
+                                                     key="time_range")
 
-                # Create two columns for buttons to be side by side
-                add_button, reset_button, download_button, empty = st.columns([1, 1, 1, 3])
-                fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
+                        # Display the selected model's parameters
+                        selected_model.display_parameters()
 
-                # Add, reset, and download buttons
-                add_button, reset_button, download_button, _ = st.columns([1, 1, 1, 3])
+                        # Display the location associated with the model on the map
+                        if selected_model.model_coordinates is not None:
+                            st.map(selected_model.model_coordinates)
 
-                with add_button:
-                    if st.button("Add to Plot", key="add_plot"):
-                        st.session_state.plot_data.append(selected_model)
+                        # Create two columns for buttons to be side by side
+                        add_button, reset_button, download_button, empty = st.columns([1, 1, 1, 3])
                         fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
+                with measurement_tab:
+                    st.markdown("## Work in progress")
 
-                with reset_button:
-                    if st.button("Reset Plot", key="reset_plot"):
-                        st.session_state.plot_data = []  # Reset the list of models
-                        fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
+                with wizard_tab:
+                    st.markdown("## Work in progress")
 
-                with download_button:
-                    download_fig = generate_plot(st.session_state.plot_data, selected_model, time_range, width=1600, height=1080)
-                    buffer = io.BytesIO()
-                    download_fig.write_image(buffer, format="png")
-                    buffer.seek(0)
-                    st.download_button(
-                        label="Download plot as PNG",
-                        data=buffer,
-                        file_name="corrosion_plot.png",
-                        mime="image/png"
-                    )
+                with selection_bottom:
+                    # Add, reset, and download buttons
+                    add_button, reset_button, download_button, _ = st.columns([1, 1, 1, 3])
+
+                    with add_button:
+                        if st.button("Add to Plot", key="add_plot"):
+                            st.session_state.plot_data.append(selected_model)
+                            fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
+
+                    with reset_button:
+                        if st.button("Reset Plot", key="reset_plot"):
+                            st.session_state.plot_data = []  # Reset the list of models
+                            fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
+
+                    with download_button:
+                        download_fig = generate_plot(st.session_state.plot_data, selected_model, time_range, width=1600,
+                                                     height=1080)
+                        buffer = io.BytesIO()
+                        download_fig.write_image(buffer, format="png")
+                        buffer.seek(0)
+                        st.download_button(
+                            label="Download plot as PNG",
+                            data=buffer,
+                            file_name="corrosion_plot.png",
+                            mime="image/png"
+                        )
 
             # Plot column displays the generated plot
             with plot_column:
