f2d2012a3aabb286d6e1896a4e35fd80364de0a0
Sven Berger <berger.sven@gmail.com>
2024-09-09 14:48:38 +0200

    Improve Plotting (#36)
    * some what working
    
    * works for 16:10
    
    * add copies of the texts
    
    * add header and footer
    
    * implement new about page
    
    * added new button pngs
    
    * hmc logo updated
    
    * hmc logo updated
    
    * hmc logo updated 3
    
    * Improve the structure of the main page
    
    * Forgot one file
    
    * Cleanup imports
    
    * more cleanup
    
    * cleaner naming
    
    * cleaner naming
    
    * cleaner naming
    
    * Update cv_aravinth.md
    
    * Update cv_daniel.md
    
    * Update cv_sven.md
    
    * Update cv_sven.md
    
    * Update cv_sven.md
    
    * remove unused file
    
    * fix imprint.md
    
    * use images for the buttons
    
    * adjust the style to the current naming scheme
    
    * formula broken
    
    * working
    
    * cleanup
    
    * rename
    
    * also load the tags
    
    * Remove hard coding
    
    * cleanup
    
    * improve the naming
    
    * cleanup
    
    * initial refactoring
    
    * improve model
    
    * improve model
    
    * fix wrong link
    
    * update
    
    * update
    
    * update model
    
    * update model
    
    * fix defaults
    
    * fix defaults
    
    * update model
    
    * fix
    
    * update model
    
    * update model
    
    * added atmopsheric and immersion corrosion pages
    
    * referecnes fixed
    
    * optimize information pages
    
    * remove article_identifer
    
    * refactor feliu1993
    
    * refactor iso9223
    
    * refactor klinesmith2007
    
    * refactor ma2010
    
    * refactor soares1999
    
    * refactor ali2020
    
    * refactor garbatov2011
    
    * refactor hicks2012
    
    * refactor kovalenko2016
    
    * cleanup
    
    * fix
    
    * remove the run functions
    
    * split out parameter_view
    
    * merge model classes
    
    * fixes
    
    * fixes
    
    * multiple plots working
    
    * multiple plots improv
    
    * multiple plots for real
    
    * working download button
    
    * Improve labeling
    
    * fix plotting issues
    
    * cleanup
    
    * cleanup
    
    * improve plotting routine
    
    * add plot_data to session state
    
    ---------
    
    Co-authored-by: Aravinth Ravikumar <aravinth.ravikumar@hereon.de>

--- web_app/model_view.py
+++ web_app/model_view.py
@@ -1,14 +1,11 @@
-import numpy as np
 import streamlit as st
-import plotly.express as px
-import streamlit.components.v1 as components
-import pandas as pd
 import io
 from models import *
-from typing import Union, List
+from typing import List
+from plot_view import generate_plot, display_plot_html
 
 # Mapping from identifiers to their respective corrosion model classes
-corwiz_models = {
+corrosion_models = {
     'model_benarie1986': Benarie1986Model,
     'model_feliu1993': Feliu1993Model,
     'din-corrosion-protection-model-iso-9223-compliant': ISO9223Model,
@@ -21,141 +18,66 @@ corwiz_models = {
     'model_kovalenko2016': Kovalenko2016Model,
 }
 
-# Keep track of all models added for plotting
-plot_data = []
-
 
 def display_model_info(model: Model) -> None:
     """Displays the model description and notes."""
     st.markdown(model.description)
     if model.special_note:
-        st.markdown("#### Model Notes: \n" + model.special_note)
+        st.markdown(f"#### Model Notes: \n{model.special_note}")
 
-
-def generate_plot(models, current_model, time_range):
-    """Generates and returns a Plotly Express figure for mass loss over time."""
-    t = np.linspace(0, time_range, 400)
-
-    # Initialize a list for plotting
-    plot_df = []
-
-    # Plot the current model's data first
-    D = current_model.evaluate_material_loss(t)
-    current_model_data = pd.DataFrame(
-        {'Time': t, 'Mass Loss': D, 'Model': f"{current_model.model_name} (live)"})
-    plot_df.append(current_model_data)
-
-    # Collect all y-values to check for max/min difference for log scaling
-    all_y_values = [D]
-
-    # Plot the additional models
-    model_counter = 1
-    for model in models:
-        D = model.evaluate_material_loss(t)
-        model_data = pd.DataFrame({'Time': t, 'Mass Loss': D, 'Model': f"{model.model_name} ({model_counter})"})
-        plot_df.append(model_data)
-        all_y_values.append(D)
-        model_counter += 1
-
-    # Combine all data into a single DataFrame
-    plot_df = pd.concat(plot_df)
-
-    # Flatten the list of y-values to check the ratio between max/min values
-    all_y_values_flat = np.concatenate(all_y_values)
-
-    # Filter out non-positive values to avoid errors with log scaling
-    positive_y_values = all_y_values_flat[all_y_values_flat > 0]
-
-    # Determine whether to use a log scale
-    if len(positive_y_values) > 0:
-        max_value = np.max(positive_y_values)
-        min_value = np.min(positive_y_values)
-        yaxis_type = "log" if max_value / min_value > 100 else "linear"
-    else:
-        yaxis_type = "linear"
-
-    px.defaults.template = "plotly_white"
-
-    # Plot using Plotly Express and use 'Model' for distinct colors and labels
-    fig = px.line(
-        plot_df,
-        x='Time',
-        y='Mass Loss',
-        color='Model',  # Use 'Model' for coloring and labeling the lines
-        labels={'Time': 'Time [years]', 'Mass Loss': 'Mass loss [um]'},
-        title="Mass Loss Over Time",
-        height=700
-    )
-
-    fig.update_layout(
-        yaxis_type=yaxis_type,
-        showlegend=True,  # Ensure the legend is displayed
-        legend_title="Models"  # Properly label the legend
-    )
-
-    return fig
-
-
-def display_plot_html(fig):
-    """Displays the generated Plotly figure as HTML in Streamlit."""
-    plot_html = fig.to_html(full_html=False, include_plotlyjs='cdn')
-    components.html(
-        f"""
-        <div style="background-color: white; padding: 10px; border-radius: 10px;">
-            {plot_html}
-        </div>
-        """,
-        height=800,
-        scrolling=True
-    )
 
 
 def display_model_view(container):
     """
     Displays the model view interface for selecting and analyzing corrosion mass loss models.
-
     Args:
         container (st.container): The Streamlit container in which the model view interface will be displayed.
     """
+    # Load available corrosion models from the directories
     model_directories = [
         '../data/kadi4mat_json/immersion_corrosion_models/',
         '../data/kadi4mat_json/atmospheric_corrosion_models/'
     ]
+    models = load_corrosion_models_from_directory(model_directories, corrosion_models)
 
-    models = load_corrosion_models_from_directory(model_directories, corwiz_models)
+    # Initialize plot_data in session state if it doesn't exist
+    if 'plot_data' not in st.session_state:
+        st.session_state.plot_data = []
 
-    global plot_data
     fig = None
 
     with container:
         st.write("---")
         st.header("Corrosion Mass Loss Models")
 
-        main_content = st.container()
-        description_content = st.container()
+        main_content, description_content = st.container(), st.container()
 
         model_process_pairs = get_corrosion_process_type(models)
 
         with main_content:
+            # Separate columns for plot and selection
             plot_column, selection_column = st.columns((1, 1))
 
             with selection_column:
+                # Selection for corrosion type
                 corrosion_types = sorted({process_type for _, process_type in model_process_pairs})
-                selected_corrosion_type = st.selectbox('**Corrosion Type**', corrosion_types,
-                                                       key="corrosion_type_selectbox")
+                selected_corrosion_type = st.selectbox('**Corrosion Type**', corrosion_types, key="corrosion_type")
 
-                filtered_models = [model for model, process_type in model_process_pairs if
-                                   process_type == selected_corrosion_type]
+                # Filter models based on the selected corrosion type
+                filtered_models = [
+                    model for model, process_type in model_process_pairs if process_type == selected_corrosion_type
+                ]
                 selected_model = st.selectbox(
                     '**Model**', filtered_models,
-                    format_func=lambda model: model.name + " (" + model.kadi_identifier + ")", key="model_selectbox"
+                    format_func=lambda model: f"{model.name} ({model.kadi_identifier})",
+                    key="model_select"
                 )
 
-                # Capture the time range input
+                # Input for time range (duration in years)
                 time_range = st.number_input('Enter duration [years]:', min_value=2.5, max_value=100.0, step=2.5,
-                                             key="time_range_input")
+                                             key="time_range")
 
-                # Display parameters and compute corrosion speed and exponent using the selected time
+                # Display the selected model's parameters
                 selected_model.display_parameters()
 
                 # Display the location associated with the model on the map
@@ -164,35 +86,40 @@ def display_model_view(container):
 
                 # Create two columns for buttons to be side by side
                 add_button, reset_button, download_button, empty = st.columns([1, 1, 1, 3])
-                fig = generate_plot(plot_data, selected_model, time_range)
+                fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
+
+                # Add, reset, and download buttons
+                add_button, reset_button, download_button, _ = st.columns([1, 1, 1, 3])
 
                 with add_button:
-                    if st.button("Add to Plot", key="add_button"):
-                        plot_data.append(selected_model)  # Add the current model to the list of models to plot
+                    if st.button("Add to Plot", key="add_plot"):
+                        st.session_state.plot_data.append(selected_model)
+                        fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
 
                 with reset_button:
-                    if st.button("Reset Plot", key="reset_button"):
-                        plot_data = []  # Reset the list of models
+                    if st.button("Reset Plot", key="reset_plot"):
+                        st.session_state.plot_data = []  # Reset the list of models
+                        fig = generate_plot(st.session_state.plot_data, selected_model, time_range)
 
                 with download_button:
-                    if fig is not None:
-                        buffer = io.BytesIO()
-                        fig.write_image(buffer, format="png")
-                        buffer.seek(0)
+                    download_fig = generate_plot(st.session_state.plot_data, selected_model, time_range, width=1600, height=1080)
+                    buffer = io.BytesIO()
+                    download_fig.write_image(buffer, format="png")
+                    buffer.seek(0)
+                    st.download_button(
+                        label="Download plot as PNG",
+                        data=buffer,
+                        file_name="corrosion_plot.png",
+                        mime="image/png"
+                    )
 
-                        st.download_button(
-                            label="Download plot as PNG",
-                            data=buffer,
-                            file_name="corrosion_plot.png",
-                            mime="image/png"
-                        )
-
+            # Plot column displays the generated plot
             with plot_column:
-                display_plot_html(fig)
+                if fig:
+                    display_plot_html(fig)
 
         with description_content:
             description_expander = st.expander("**Model Description**", expanded=False)
-
             with description_expander:
                 display_model_info(selected_model)
 
