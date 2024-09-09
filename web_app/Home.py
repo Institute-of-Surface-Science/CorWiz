fe3b837a897412fb8bbceb73738b02f05395d5c0
Sven Berger <berger.sven@gmail.com>
2024-09-09 14:20:05 +0200

    remove measurement_view

--- web_app/Home.py
+++ web_app/Home.py
@@ -3,14 +3,9 @@ import streamlit as st
 from header import add_header
 from footer import add_footer
 from model_view import display_model_view
-from measurement_view import display_measurement_view
 
 add_header()
 
 model_view_container = st.container()
 display_model_view(model_view_container)
-
-measurement_view_container = st.container()
-display_measurement_view(measurement_view_container)
-
 add_footer()
